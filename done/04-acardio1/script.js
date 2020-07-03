6ef009d96f9f414f4bdb663689b991c8eec4c015
Emilio <emilio.hellberg@hyperisland.se>
2020-07-03 14:02:11 +0200

    Completed 04

--- done/04-acardio1/script.js
+++ done/04-acardio1/script.js
@@ -23,18 +23,18 @@ const people = ['Beethoven, Ludwig', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 
-let bornDate = inventors.filter((x) => x.year > 1499 && x.year < 1600);
+// let bornDate = inventors.filter((x) => x.year > 1499 && x.year < 1600);
 
-console.table(bornDate);
+// console.table(bornDate);
 
-const fifteen = inventors.filter(function (inventor) {
-  if (inventor.year >= 1500 && inventor.year < 1600) {
-    return true;
-    {
-    }
-  }
-});
-console.table(fifteen)
+// const fifteen = inventors.filter(function (inventor) {
+//   if (inventor.year >= 1500 && inventor.year < 1600) {
+//     return true;
+//     {
+//     }
+//   }
+// });
+// console.table(fifteen)
 
 //The filter function - does what it says on the tin - it filters out the objects in the array that does not match our conditions.
 
@@ -45,6 +45,12 @@ console.table(fifteen)
 
 // console.log(firstLast);
 
+// const fullNames = inventors.map(
+//   (inventor) => `${inventor.first} ${inventor.last}`
+// );
+
+// console.log(fullNames)
+
 //The map function - goes thru the array and takes out the values from that correcpondes to our conditions and creates a new array with these values.
 
 // Array.prototype.sort()
@@ -52,7 +58,26 @@ console.table(fifteen)
 
 // let birthOldYoung = inventors.sort((a, b) => a.year - b.year);
 
-// console.log(birthOldYoung);
+// console.table(birthOldYoung);
+
+// const ordered = inventors.sort(function (a, b) {
+//   console.log(a.year + " a.year");
+//   console.log(b.year + " b.year");
+//   if (a.year > b.year) {
+//     return 1 + console.log(1);
+//   } else {
+//     return -1 + console.log(0);
+//   }
+// });
+
+// const ordered = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
+
+// console.table(ordered);
+
+// const testSort = [1, 4, 7, 3, 5, 2, 6, 8, 9];
+
+// console.log(testSort);
+// console.log(testSort.sort())
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
@@ -61,12 +86,33 @@ console.table(fifteen)
 // let collectiveYears = inventors.reduce((total, amount) => total + (amount.passed - amount.year), 0);
 // console.log(collectiveYears);
 
+// const totalYear = inventors.reduce((total, inventor) => {
+//   return total + (inventor.passed - inventor.year);
+// }, 0);
+
+// console.log(totalYear);
+
 // 5. Sort the inventors by years lived
 
 // prettier-ignore
 
 // let yearsLived = inventors.sort((a, b) => (b.passed - b.year) - (a.passed - a.year))
 // console.table(yearsLived);
+
+// const oldest = inventors.sort(function (a, b) {
+//   const lastGuy = a.passed - a.year;
+//   const nextGuy = b.passed - b.year;
+
+//   return lastGuy > nextGuy ? -1 : 1;
+
+// if(lastGuy > nextGuy) {
+//   return -1;
+// } else {
+//   return 1;
+// }
+// });
+
+// console.table(oldest)
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
@@ -81,10 +127,18 @@ console.table(fifteen)
 // 7. sort Exercise
 // Sort the people alphabetically by last name
 
-// console.log(people)
+// console.table(people)
 
 // const lastName = people.sort((a, b) => a < b);
-// console.log(lastName);
+// console.table(lastName);
+
+// const alpha = people.sort(function (lastOne, nextOne) {
+//   const [alast, afirst] = lastOne.split(", ");
+//   const [blast, bfirst] = nextOne.split(", ");
+//   return alast > blast ? 1 : -1;
+// });
+
+// console.table(alpha)
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
@@ -101,3 +155,14 @@ console.table(fifteen)
 // }, {});
 
 // console.log(data2);
+
+// const transportation = data.reduce(function (obj, item) {
+//   // console.log(item);
+//   if (!obj[item]) {
+//     obj[item] = 0;
+//   }
+//   obj[item]++;
+//   return obj;
+// }, {});
+
+// console.log(transportation);
