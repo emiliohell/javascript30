190e0cf83b602d68aeccbd33170b88b9103312c7
Emilio <emilio.hellberg@hyperisland.se>
2020-07-06 21:31:51 +0200

    Started 05

---
+++ 05-flex/script.js
@@ -0,0 +1,24 @@
+function panelFunc1() {
+  var element = document.getElementById("panels1");
+  element.classList.toggle("click");
+}
+
+function panelFunc2() {
+  var element = document.getElementById("panels2");
+  element.classList.toggle("click");
+}
+
+function panelFunc3() {
+  var element = document.getElementById("panels3");
+  element.classList.toggle("click");
+}
+
+function panelFunc4() {
+  var element = document.getElementById("panels4");
+  element.classList.toggle("click");
+}
+
+function panelFunc5() {
+  var element = document.getElementById("panels5");
+  element.classList.toggle("click");
+}
