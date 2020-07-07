96ec3f620abe7ab356b02f60de8704c589c392c6
Emilio <emilio.hellberg@hyperisland.se>
2020-07-07 14:35:26 +0200

    Finished 05 - Pretty bad result but working

--- 05-flex/script.js
+++ 05-flex/script.js
@@ -1,24 +1,39 @@
 function panelFunc1() {
   var element = document.getElementById("panels1");
+  var element2 = document.getElementById("panelTitle1");
   element.classList.toggle("click");
+  element2.classList.toggle("clickText");
+  element2.classList.toggle("marginText");
 }
 
 function panelFunc2() {
   var element = document.getElementById("panels2");
+  var element2 = document.getElementById("panelTitle2");
   element.classList.toggle("click");
+  element2.classList.toggle("clickText");
+  element2.classList.toggle("marginText");
 }
 
 function panelFunc3() {
   var element = document.getElementById("panels3");
+  var element2 = document.getElementById("panelTitle3");
   element.classList.toggle("click");
+  element2.classList.toggle("clickText");
+  element2.classList.toggle("marginText");
 }
 
 function panelFunc4() {
   var element = document.getElementById("panels4");
+  var element2 = document.getElementById("panelTitle4");
   element.classList.toggle("click");
+  element2.classList.toggle("clickText");
+  element2.classList.toggle("marginText");
 }
 
 function panelFunc5() {
   var element = document.getElementById("panels5");
+  var element2 = document.getElementById("panelTitle5");
   element.classList.toggle("click");
+  element2.classList.toggle("clickText");
+  element2.classList.toggle("marginText");
 }
